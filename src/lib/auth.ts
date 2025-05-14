ab8bb9fd182db0facba631eb7c521dc8bb25af08
TonioCodeur <a.copiercoller@gmail.com>
2025-05-14 17:06:05 +0200

    Ajout de la configuration de VSCode et mise à jour de l'implémentation de l'authentification avec NextAuth en remplaçant l'adaptateur PostgreSQL par PrismaAdapter. Amélioration de la gestion de l'instance Prisma dans l'environnement global.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,16 @@
-import { NextAuthOptions } from "next-auth";
-import { Pool } from "@neondatabase/serverless";
-import GoogleProvider from "next-auth/providers/google";
-import PostgresAdapter from "@auth/pg-adapter";
-
-const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
-export const runtime = 'nodejs';
-// Configuration séparée des options
-export const authOptions: NextAuthOptions = {
-  adapter: PostgresAdapter(pool),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { prisma } from "./prisma"
+ 
+export const authOptions = NextAuth( {
+  // Create a `Pool` inside the request handler.
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-}
+      }),
+    ],
+  }
+)
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,10 @@
+import { PrismaClient } from '@prisma/client'
 
-// Importation correcte du PrismaClient
-import { PrismaClient } from "@prisma/client"
-
+// Définition du type global pour Prisma
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
+// Création d'une instance Prisma ou réutilisation de l'instance existante
 export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+// Assignation de l'instance à la variable globale en développement
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
